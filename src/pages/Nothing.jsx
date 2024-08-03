ac91e74f84c943921d766eb852991d65ee8ea16c
AkashSharma923 <akashsharma923b@gmail.com>
2024-08-04 02:38:08 +0530

    minor changes

--- src/pages/Nothing.jsx
+++ src/pages/Nothing.jsx
@@ -18,7 +18,6 @@ function Nothing() {
   
       useEffect(() => {
                  
-      console.log(leftSide, rightSide, topSide, bottomSide)
          setright(rightSide)
          settop(topSide)
          setbottom(bottomSide)
@@ -28,7 +27,7 @@ function Nothing() {
   return (
       
     <div className='text-[40px] bg-[#46753f] w-[60%] rounded-3xl h-[500px] flex justify-between items-center text-center'>
-     <div className='w-fit h-full flex justify-center  items-center'><img className='rounded-tl-3xl rounded-bl-3xl w-full h-full ' src="/src/assets/newerror.png" alt="" /></div>
+     <div className='w-fit h-full flex justify-center  items-center'><img className='rounded-tl-3xl rounded-bl-3xl w-full h-full ' src="/newerror.png" alt="" /></div>
      <div className='text-white w-[60%]  h-full relative  rounded-tr-3xl rounded-br-3xl bg-gradient-to-r from-purple-500 to-purple-900 flex justify-center items-center text-center'>
      <div id='colour' onChange={increase()} style={{borderRadius:`${left}% ${right}% ${top}% ${bottom}% `}} className='w-[100px] h-[100px] transition-all bg-opacity-50 bg-gradient-to-r from-purple-500 to-purple-900 absolute top-10 right-40 '></div>
      <div id='colour' onChange={increase()} style={{borderRadius:`${left}% ${right}% ${top}% ${bottom}% `}} className='w-[50px] h-[50px] transition-all bg-opacity-50 bg-gradient-to-r from-purple-500 to-purple-900 absolute top-[400px] right-20 '></div>
